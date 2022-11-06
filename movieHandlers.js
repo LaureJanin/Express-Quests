d1ee286a9c9e0d8636129c16b05edd35a68b6cef
LAURE JANIN <ljanin51@gmail.com>
2022-11-06 13:21:28 +0100

    Création de la function delete

--- movieHandlers.js
+++ movieHandlers.js
@@ -67,9 +67,31 @@ const updateMovie = (req, res) => {
     });
 }
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query(
+      "DELETE FROM movies where id = ?",
+      [id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+}
+
 module.exports = {
   getMovies,
   getMovieById,
   postMovie,
   updateMovie,
+  deleteMovie
 };
